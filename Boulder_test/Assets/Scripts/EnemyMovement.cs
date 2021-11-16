e68645e8a294ea5a39b3c9a8d34831d43e443136
Amanda <amanda.vanhatalo@tuni.fi>
2021-11-16 16:39:33 +0200

    Second version of enemy AI
    Most likely will change

--- Boulder_test/Assets/Scripts/EnemyMovement.cs
+++ Boulder_test/Assets/Scripts/EnemyMovement.cs
@@ -15,12 +15,12 @@ public class EnemyMovement : MonoBehaviour
         nextPos = transform.position;
 
         System.Random randomNumber = new System.Random();
-        int generateRandom = randomNumber.Next(1, 5);
-        Debug.Log("Number" + generateRandom);
-        // 1=up, 2=right, 3=down, 4=left
+        //int generateRandom = randomNumber.Next(1, 5);
+        //Debug.Log("Number" + generateRandom);
+        // 1=up, 2=right, 3=down, 4=left (also)5=up
 
 
-        if (generateRandom == 1)
+        /*if (generateRandom == 1)
         {
             nextPos.y += 1f;
         }
@@ -35,6 +35,54 @@ public class EnemyMovement : MonoBehaviour
         else if (generateRandom == 4)
         {
             nextPos.x -= 1f;
+        }*/
+
+        float playerPositionX = GameObject.FindGameObjectWithTag("Player").transform.position.x;
+        float playerPositionY = GameObject.FindGameObjectWithTag("Player").transform.position.y;
+
+        if (playerPositionX >= 0 && playerPositionY >= 0) {
+            int generateRandom = randomNumber.Next(1, 3);
+            if (generateRandom == 1)
+            {
+                nextPos.y += 1f;
+            }
+            else if (generateRandom == 2)
+            {
+                nextPos.x += 1f;
+            }
+        }
+        else if(playerPositionX >= 0 && playerPositionY <= 0) {
+            int generateRandom = randomNumber.Next(2, 4);
+            if (generateRandom == 2)
+            {
+                nextPos.x += 1f;
+            }
+            else if (generateRandom == 3)
+            {
+                nextPos.y -= 1f;
+            }
+        }
+        else if (playerPositionX <= 0 && playerPositionY <= 0) {
+            int generateRandom = randomNumber.Next(3, 5);
+            if (generateRandom == 3)
+            {
+                nextPos.y -= 1f;
+            }
+            else if (generateRandom == 4)
+            {
+                nextPos.x -= 1f;
+            }
+        }
+        else {
+            int generateRandom = randomNumber.Next(4, 6);
+            if (generateRandom == 4)
+            {
+                nextPos.x -= 1f;
+            }
+            else if (generateRandom == 5)
+            {
+                nextPos.y += 1f;
+            }
         }
 
         moveTimer += Time.deltaTime;
